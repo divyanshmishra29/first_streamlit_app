f5428c8d389a48186fc0d6988448fce1b9db75b8
Divyansh Mishra <divyanshmishra29@gmail.com>
2023-09-28 19:31:55 +0530

    Update streamlit_app.py
    dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,9 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit list contains:")
-streamlit.text(my_data_row)
+my_data_row = my_cur.fetchall()
+streamlit.text("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
 
 
 
